432ea422c2aa2c8f5d7758efc8ee10eb73f631dd
Leopoldo Miracco <90115176+leomiracco@users.noreply.github.com>
2022-04-02 16:53:23 -0300

    Test thirty-one solved the mobile image problem.

--- 02-Landing/js/main.js
+++
@@ -1,8 +0,0 @@
-import resize from "./resize_img.js";
-
-const d = document;
-
-d.addEventListener("DOMContentLoaded", (e) => {
-  console.log("Se inició correctamente...");
-  resize(".container-layer", ".main-content");
-});
